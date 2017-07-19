40a53d74b2a43ab556ee53e92e5479da4112a3ed
hyperparticle <hyperparticle@gmail.com>
2017-07-18 22:11:32 -0600

    Change default cursor

---
+++ Assets/Scripts/View/Game/CursorView.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace View.Game
+{
+	public class CursorView : MonoBehaviour {
+
+		public Texture2D CursorTexture;
+
+		private const CursorMode CursorMode = UnityEngine.CursorMode.Auto;
+		private readonly Vector2 _hotSpot = Vector2.zero;
+
+		private void Start() {
+			Cursor.SetCursor(CursorTexture, _hotSpot, CursorMode);
+		}
+	
+	}
+}
