1f6128c776ef63ca2c1c6fdd2a29465fa4febc80
hyperparticle <hyperparticle@gmail.com>
2017-10-21 01:07:33 +0200

    Add animating logo

---
+++ Assets/Scripts/View/Items/SimpleRotator.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace View.Items
+{
+	public class SimpleRotator : MonoBehaviour
+	{
+		// TODO: make configurable
+		public float Delay = 1f;
+		public float Time = 5f;
+
+		public LeanTweenType Ease = LeanTweenType.easeOutBack;
+		
+		public Vector3 StartRotation;
+		public Vector3 EndRotation;
+
+		private void Start()
+		{
+			Ease = LeanTweenType.easeInOutSine;
+			transform.localEulerAngles = -StartRotation;
+			
+			LeanTween.rotateLocal(gameObject, EndRotation, Time)
+				.setDelay(Delay)
+				.setEase(Ease);
+		}
+	}
+}
