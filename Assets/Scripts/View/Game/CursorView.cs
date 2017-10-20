7359828220935001e52c8112c73a93ba2210db51
hyperparticle <hyperparticle@gmail.com>
2017-10-20 12:43:50 +0200

    Change cursor when mouse is pressed

--- Assets/Scripts/View/Game/CursorView.cs
+++ Assets/Scripts/View/Game/CursorView.cs
@@ -5,6 +5,7 @@ namespace View.Game
 	public class CursorView : MonoBehaviour {
 
 		public Texture2D CursorTexture;
+		public Texture2D CursorTextureHot;
 
 		private const CursorMode CursorMode = UnityEngine.CursorMode.Auto;
 
@@ -12,6 +13,16 @@ namespace View.Game
 			var hotSpot = new Vector2 (CursorTexture.width, CursorTexture.height) / 2f;
 			Cursor.SetCursor(CursorTexture, hotSpot, CursorMode);
 		}
-	
+
+		private void Update()
+		{
+			if (Input.GetKeyDown(KeyCode.Mouse0)) {
+				var hotSpot = new Vector2(CursorTextureHot.width, CursorTextureHot.height) / 2f;
+				Cursor.SetCursor(CursorTextureHot, hotSpot, CursorMode);
+			} else if (Input.GetKeyUp(KeyCode.Mouse0)) {
+				var hotSpot = new Vector2(CursorTexture.width, CursorTexture.height) / 2f;
+				Cursor.SetCursor(CursorTexture, hotSpot, CursorMode);
+			}
+		}
 	}
 }
